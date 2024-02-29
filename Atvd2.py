373500a8813d2fbbc92b721571e0081f11c5620e
Henryzzin <131017175+Henryzzin@users.noreply.github.com>
2024-02-29 17:35:00 +0000

    Atividade2

---
+++ Atvd2.py
@@ -0,0 +1,11 @@
+A=20
+B=10
+soma=A+B
+subtracao=A-B
+divisao=A/B
+print("A soma A+B é igual à:",soma,". A subtração A-B é igual à:", subtracao,". A divisão A/B é igual à:", divisao,".")
+
+C=A
+A=B
+B=C
+print("O valor de A é:",A,"e o valor de B é:",B,")
